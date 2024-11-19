69b12a22c5397dcf5b8f95d7ad703634f244ebaf
muhammad.atmojo <muhammad.atmojo@bytedance.com>
2024-11-19 11:05:01 +0700

    adapt to hit ip address nsq

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/muhfdll/script_nsq
 
 go 1.19
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -2,47 +2,56 @@ package main
 
 import (
 	"fmt"
-	"os/exec"
+	"net/http"
+	"os"
+	"strings"
 	"time"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	// Variabel hosts
-	hosts := []string{
-		"10.41.144.107:4151",
-		// "2",
-		// "3",
-
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("Error loading .env file: %s\n", err)
+		return
 	}
 
-	// Loop tak berujung untuk menjalankan perintah setiap 10 detik
+	hostsEnv := os.Getenv("HOSTS")
+	params := os.Getenv("PARAMS")
+	methodEnv := os.Getenv("METHOD")
+	if hostsEnv == "" || params == "" {
+		fmt.Println("Check the env!")
+		return
+	}
+
+	hosts := strings.Split(hostsEnv, ",")
+
 	for {
-		fmt.Println("Starting to execute commands...")
+		fmt.Println("============Starting to hit endpoints============")
 
-		// Loop untuk setiap host
 		for _, host := range hosts {
-			// Buat URL endpoint berdasarkan host
-			url := fmt.Sprintf("http://%s/channel/empty?topic=promocatalog__sync_user_promo_coupon_v2&channel=promocatalog__sync", host)
-			// url := fmt.Sprintf("https://jsonplaceholder.typicode.com/posts/%s", host)
+			url := fmt.Sprintf("http://%s/channel/empty?%s", host, params)
 
-			// Command curl
-			command := fmt.Sprintf("curl --location --request POST %s", url)
-			fmt.Printf("Executing: %s\n", command)
+			client := &http.Client{}
+			req, err := http.NewRequest(methodEnv, url, nil)
 
-			// Eksekusi command menggunakan exec.Command
-			cmd := exec.Command("sh", "-c", command)
-			output, err := cmd.CombinedOutput()
-
-			// Tampilkan output atau error
 			if err != nil {
-				fmt.Printf("Error executing command for host %s: %s\n", host, err)
-			} else {
-				fmt.Printf("Output for host %s:\n%s\n", host, string(output))
+				fmt.Println(err)
+				return
 			}
+			res, err := client.Do(req)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			defer res.Body.Close()
 
-			fmt.Println("------------------------------") // Pemisah antar command
+			fmt.Printf("\nResponse Code from %s : %v \n\n", url, res.StatusCode)
+			fmt.Println("====================================")
 		}
-		fmt.Println("All commands executed. Waiting for 10 seconds...\n")
-		time.Sleep(10 * time.Second) // Tunggu 10 detik sebelum iterasi berikutnya
+
+		fmt.Println("All endpoints hit. Waiting for 10 seconds...")
+		time.Sleep(10 * time.Second)
 	}
 }
